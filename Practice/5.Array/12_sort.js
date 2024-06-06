ed810b81a8364d355cbd2f6acba4c715d5ed2197
PHOK KEOMONNYRATANAK <rattanakphok9@gmail.com>
2024-06-06 13:08:50 +0700

    updated Array

--- Practice/5.Array/12_sort.js
+++ Practice/5.Array/12_sort.js
@@ -12,9 +12,16 @@ const course = [
 
 course.sort(function(a,b){
 
-if(a.name < b.name) return -1;
-if(a.name > b.name) return 1
+// a < b => -1
+// a > b => 1
+// a === b => 0
 
+const nameA = a.name.toLowerCase()
+const nameB = b.name.toLowerCase()
+
+if(nameA < nameB) return -1;
+if(nameA > nameB) return 1
+return 0
 });
 
 console.log(course)
